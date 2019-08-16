39d9374fd525ffa40a4eca63703dec5012ac2374
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2019-08-16 19:25:25 +0200

    Updates

---
+++ precache-manifest.facdc10f5c51574008891ab078952ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4b2934d19ec627310ae6dbcb0dfef5",
+    "url": "/bubbles/index.html"
+  },
+  {
+    "revision": "9200f1a71803a8162a4e",
+    "url": "/bubbles/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7c91ab6ec7c81b02854c",
+    "url": "/bubbles/static/js/2.e78ab8b5.chunk.js"
+  },
+  {
+    "revision": "9200f1a71803a8162a4e",
+    "url": "/bubbles/static/js/main.8335cd9a.chunk.js"
+  },
+  {
+    "revision": "9d4ad0a0c5ee3d10a5f7",
+    "url": "/bubbles/static/js/runtime~main.b16bd637.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bubbles/static/media/logo.5d5d9eef.svg"
+  }
+]);
